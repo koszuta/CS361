b9371f1269f4f00064cefefaa5ce9519565fa058
koszuta <koszutan94@gmail.com>
2015-11-09 20:29:18 -0600

    Revert "Moved Instructor Handlers to "instructor.py""
    This reverts commit 48fed60514421bc40e5cd18247d456079d96be88.

--- project/Instructor.py
+++ project/Instructor.py
@@ -1,12 +1,6 @@
-import os
-import webapp2
-import urllib
-from jinja2 import Environment, FileSystemLoader
-from google.appengine.ext import ndb
-
 import hours
-import syllabus
-import user
+
+from google.appengine.ext import ndb
 
 class Instructor(ndb.Model):
     first = ndb.StringProperty(required=True)
@@ -20,66 +14,26 @@ class Instructor(ndb.Model):
     
     def key(self):
         return str(self.last + ", " + self.first);
-
-           
-class AddHandler(webapp2.RequestHandler):
-    def post(self):
-        option = self.request.get("instructorToAddButton")
-        selected = self.request.get("availableInstructors")
-        chosen = Instructor(first="Boner", last="Butts")
         
-        for item in user.savedInstructors:
-            if item.key() == selected:
-                chosen = item
+'''       
+    def setFirstName(self, str):
+        self.first = str;
         
-        if option == "Add":
-            syl.instructors.append(chosen)
+    def setLastName(self, str):
+        self.last = str;
         
-        chosen.isSelected = True        
+    def setEmail(self, str):
+        self.email = str;
         
-        self.redirect('/#administratorViewInstructorInfoMain')
+    def setPhone(self, str):
+        self.phone = str;
         
-
-class RemoveHandler(webapp2.RequestHandler):        
-    def post(self):
-        selected = self.request.get("selectedInstructors")
-        chosen = instructor.Instructor(first="Butts", last="Boner")
+    def setBuilding(self, str):
+        self.building = str;
         
-        for item in syl.instructors:
-            if item.key() == selected:
-                chosen=item
-                syl.instructors.remove(item) 
-                
-        chosen.isSelected = True  
-           
-        self.redirect('/#administratorViewInstructorInfoMain')
+    def setRoom(self, str):
+        self.room = str;
         
-       
-class EditHandler(webapp2.RequestHandler):
-    def post(self):
-        option = self.request.get("editInstructorSubmit")
-        myfirst = self.request.get("instructorFirstName")
-        mylast = self.request.get("instrcutorLastName")
-        myemail = self.request.get("instructorEmail")
-        myphone = self.request.get("instructorPhone")
-        mybuilding = self.request.get("instructorBuildingSelect")
-        myroom = self.request.get("instructorOfficeRoom")
-        
-        chosen = instructor.Instructor(first="Boner", last="Butts")
-        
-        for item in syl.instructors:
-            if item.isSelected:
-                chosen=item
-                
-        if option == "Update Info":
-            chosen.first = myfirst
-            chosen.last = mylast
-            chosen.email = myemail
-            chosen.phone = myphone
-            chosen.building = mybuilding
-            chosen.room = myroom
-            
-        elif option == "Create New":
-            user.savedInstructors.append(instructor.Instructor(first = myfirst, last = mylast, email = myemail, phone = myphone, building = mybuilding, room = myroom))
-
-        self.redirect('/#administratorViewInstructorInfoMain')
+    def setHours(self, str):
+        self.hours = str;
+'''
--- project/main.py
+++ project/main.py
@@ -61,12 +61,75 @@ class MainHandler(webapp2.RequestHandler):
         
         self.response.write(template.render(context))
 
+           
+class AddHandler(webapp2.RequestHandler):
+    def post(self):
+        option = self.request.get("instructorToAddButton")
+        selected = self.request.get("availableInstructors")
+        chosen = instructor.Instructor(first="Boner", last="Butts")
+        
+        for item in user.savedInstructors:
+            if item.key() == selected:
+                chosen = item
+        
+        if option == "Add":
+            syl.instructors.append(chosen)
+        
+        chosen.isSelected = True        
+        
+        self.redirect('/#administratorViewInstructorInfoMain')
+        
+
+class RemoveHandler(webapp2.RequestHandler):        
+    def post(self):
+        selected = self.request.get("selectedInstructors")
+        chosen = instructor.Instructor(first="Butts", last="Boner")
+        
+        for item in syl.instructors:
+            if item.key() == selected:
+                chosen=item
+                syl.instructors.remove(item) 
+                
+        chosen.isSelected = True  
+           
+        self.redirect('/#administratorViewInstructorInfoMain')
+        
+       
+class EditHandler(webapp2.RequestHandler):
+    def post(self):
+        option = self.request.get("editInstructorSubmit")
+        myfirst = self.request.get("instructorFirstName")
+        mylast = self.request.get("instrcutorLastName")
+        myemail = self.request.get("instructorEmail")
+        myphone = self.request.get("instructorPhone")
+        mybuilding = self.request.get("instructorBuildingSelect")
+        myroom = self.request.get("instructorOfficeRoom")
+        
+        chosen = instructor.Instructor(first="Boner", last="Butts")
+        
+        for item in syl.instructors:
+            if item.isSelected:
+                chosen=item
+                
+        if option == "Update Info":
+            chosen.first = myfirst
+            chosen.last = mylast
+            chosen.email = myemail
+            chosen.phone = myphone
+            chosen.building = mybuilding
+            chosen.room = myroom
+            
+        elif option == "Create New":
+            user.savedInstructors.append(instructor.Instructor(first = myfirst, last = mylast, email = myemail, phone = myphone, building = mybuilding, room = myroom))
+
+        self.redirect('/#administratorViewInstructorInfoMain')
+        
         
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/addinstructor', instructor.AddHandler),
-    ('/removeinstructor', instructor.RemoveHandler),
-    ('/editinstructor', instructor.EditHandler),
+    ('/addinstructor', AddHandler),
+    ('/removeinstructor', RemoveHandler),
+    ('/editinstructor', EditHandler),
     ('/editbooks', textbook.TextbookHandler),
     ('/editbook', textbook.EditTextbookHandler),
     ('/removebooks', textbook.RemoveTextbookHandler),
