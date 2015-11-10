48fed60514421bc40e5cd18247d456079d96be88
koszuta <koszutan94@gmail.com>
2015-11-09 19:40:34 -0600

    Moved Instructor Handlers to "instructor.py"

--- project/Instructor.py
+++ project/Instructor.py
@@ -1,6 +1,12 @@
-import hours
-
+import os
+import webapp2
+import urllib
+from jinja2 import Environment, FileSystemLoader
 from google.appengine.ext import ndb
+
+import hours
+import syllabus
+import user
 
 class Instructor(ndb.Model):
     first = ndb.StringProperty(required=True)
@@ -14,26 +20,66 @@ class Instructor(ndb.Model):
     
     def key(self):
         return str(self.last + ", " + self.first);
+
+           
+class AddHandler(webapp2.RequestHandler):
+    def post(self):
+        option = self.request.get("instructorToAddButton")
+        selected = self.request.get("availableInstructors")
+        chosen = Instructor(first="Boner", last="Butts")
         
-'''       
-    def setFirstName(self, str):
-        self.first = str;
+        for item in user.savedInstructors:
+            if item.key() == selected:
+                chosen = item
         
-    def setLastName(self, str):
-        self.last = str;
+        if option == "Add":
+            syl.instructors.append(chosen)
         
-    def setEmail(self, str):
-        self.email = str;
+        chosen.isSelected = True        
         
-    def setPhone(self, str):
-        self.phone = str;
+        self.redirect('/#administratorViewInstructorInfoMain')
         
-    def setBuilding(self, str):
-        self.building = str;
+
+class RemoveHandler(webapp2.RequestHandler):        
+    def post(self):
+        selected = self.request.get("selectedInstructors")
+        chosen = instructor.Instructor(first="Butts", last="Boner")
         
-    def setRoom(self, str):
-        self.room = str;
+        for item in syl.instructors:
+            if item.key() == selected:
+                chosen=item
+                syl.instructors.remove(item) 
+                
+        chosen.isSelected = True  
+           
+        self.redirect('/#administratorViewInstructorInfoMain')
         
-    def setHours(self, str):
-        self.hours = str;
-'''
+       
+class EditHandler(webapp2.RequestHandler):
+    def post(self):
+        option = self.request.get("editInstructorSubmit")
+        myfirst = self.request.get("instructorFirstName")
+        mylast = self.request.get("instrcutorLastName")
+        myemail = self.request.get("instructorEmail")
+        myphone = self.request.get("instructorPhone")
+        mybuilding = self.request.get("instructorBuildingSelect")
+        myroom = self.request.get("instructorOfficeRoom")
+        
+        chosen = instructor.Instructor(first="Boner", last="Butts")
+        
+        for item in syl.instructors:
+            if item.isSelected:
+                chosen=item
+                
+        if option == "Update Info":
+            chosen.first = myfirst
+            chosen.last = mylast
+            chosen.email = myemail
+            chosen.phone = myphone
+            chosen.building = mybuilding
+            chosen.room = myroom
+            
+        elif option == "Create New":
+            user.savedInstructors.append(instructor.Instructor(first = myfirst, last = mylast, email = myemail, phone = myphone, building = mybuilding, room = myroom))
+
+        self.redirect('/#administratorViewInstructorInfoMain')
