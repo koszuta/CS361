ffc01c8c4a135a9a033e9b75a1957b229c485452
koszuta <koszutan94@gmail.com>
2015-11-18 16:04:28 -0600

    Moved Handlers out of Main.py

--- project/instructor.py
+++ project/instructor.py
@@ -23,3 +23,102 @@ class Instructor(ndb.Model):
         
     def copy(self):
         return Instructor(first=self.first, last=self.last, email=self.email, phone=self.phone, building=self.building, room=self.room, isSelected=self.isSelected)
+
+        
+class AddHandler(webapp2.RequestHandler):
+    def post(self):
+        option = self.request.get("instructorToAddButton")
+        selected = self.request.get("availableInstructors")
+        chosen = instructor.Instructor()
+        
+        for item in user.savedInstructors:
+            if item.key() == selected:
+                chosen = item
+            item.isSelected = False
+        
+        if option == "Add":
+            syl.instructors.append(chosen)
+        
+        chosen.isSelected = True
+        
+        chosen.put()
+        syl.put()
+        self.redirect('/editinstructor')
+        
+
+class RemoveHandler(webapp2.RequestHandler):        
+    def post(self):
+        selected = self.request.get("selectedInstructors")
+        chosen = instructor.Instructor()
+        
+        for item in syl.instructors:
+            if item.key() == selected:
+                chosen=item
+                syl.instructors.remove(item) 
+            item.isSelected = False
+                
+        chosen.isSelected = True  
+        
+        syl.put()   
+        self.redirect('/editinstructor')
+        
+       
+class EditHandler(webapp2.RequestHandler):
+    def get(self):
+        x = instructor.Instructor()
+        for item in user.savedInstructors:
+            if item.isSelected:
+                x = item.copy()
+            
+        template = template_env.get_template('instructorEdit.html')
+        
+        context = {
+            'savedInstructors': user.savedInstructors,
+            'syllabusInstructors': syl.instructors,
+            'selected': x.key(),
+            'sel_first': x.first,
+            'sel_last': x.last,
+            'sel_email': x.email,
+            'sel_phone': x.phone,
+            'sel_building': x.building,
+            'sel_room': x.room,
+            'sel_hours': x.hours,
+        }
+
+        self.response.write(template.render(context))
+        
+    def post(self):
+        option = self.request.get("editInstructorSubmit")
+        
+        myfirst = self.request.get("instructorFirstName")
+        mylast = self.request.get("instructorLastName")
+        myemail = self.request.get("instructorEmail")
+        myphone = self.request.get("instructorPhone")
+        mybuilding = self.request.get("instructorBuildingSelect")
+        myroom = self.request.get("instructorOfficeRoom")
+        
+        chosen = instructor.Instructor()
+                
+        if option == "Update Info":
+            for item in user.savedInstructors:
+                if item.isSelected:
+                    item.first = myfirst
+                    item.last = mylast
+                    item.email = myemail
+                    item.phone = myphone
+                    item.building = mybuilding
+                    item.room = myroom
+        
+        elif option == "Create New":
+            chosen.first = myfirst
+            chosen.last = mylast
+            chosen.email = myemail
+            chosen.phone = myphone
+            chosen.building = mybuilding
+            chosen.room = myroom
+            
+            user.savedInstructors.append(chosen) 
+
+            chosen.put()
+        syl.put()
+        self.redirect('/editinstructor')
--- project/policy.py
+++ project/policy.py
@@ -13,3 +13,86 @@ class Policy(ndb.Model):
 			return "none"
 			
 		return self.title
+	
+		        
+class EditHandler(webapp2.RequestHandler):
+    def get(self):
+        x = policy.Policy()
+        for item in user.savedPolicies:
+            if item.isSelected:
+                x = item.copy()
+            
+        template = template_env.get_template('policyEdit.html')
+        
+        context = {
+            'savedPolicies': user.savedPolicies,
+            'selected': x.key(),
+            'policies': syl.policies,
+            'title': x.title,
+            'description': x.description,
+        }
+
+        self.response.write(template.render(context))
+        
+    def post(self):
+        option = self.request.get("policyEditorButton")
+        
+        mytitle = self.request.get("policyTitle")
+        mydescription = self.request.get("policyDescription")
+        
+        chosen = policy.Policy()
+                
+        if option == "Update":
+            for item in user.savedPolicies:
+                if item.isSelected:
+                    item.title = mytitle
+                    item.description = mydescription
+                    
+        elif option == "Create New":
+            chosen.title = mytitle
+            chosen.description = mydescription
+            
+            user.savedPolicies.append(chosen) 
+
+            chosen.put()
+            
+        syl.put()
+        self.redirect('/editpolicy')
+        
+
+class AddHandler(webapp2.RequestHandler):
+    def post(self):
+        option = self.request.get("savedPolicyButton")
+        selected = self.request.get("savedpolicies")
+        chosen = policy.Policy()
+        
+        for item in user.savedPolicies:
+            if item.key() == selected:
+                chosen = item
+            item.isSelected = False
+        
+        if option == "Add":
+            syl.policies.append(chosen)
+        
+        chosen.isSelected = True
+        
+        chosen.put()
+        syl.put()
+        self.redirect("/editpolicy")
+        
+        
+class RemoveHandler(webapp2.RequestHandler):
+    def post(self):
+        selected = self.request.get("policiesOnSyllabus")
+        chosen = policy.Policy()
+        
+        for item in syl.policies:
+            if item.key() == selected:
+                chosen=item
+                syl.policies.remove(item) 
+            item.isSelected = False
+                
+        chosen.isSelected = True  
+           
+        syl.put()
+        self.redirect("/editpolicy")
