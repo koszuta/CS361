b4ea733b6aa0f951d7d49cac75912d0e6e3c7d8e
Scott Ehlert <ds@alliedmods.net>
2015-12-05 17:25:18 -0600

    Fix logout functionality and really remove user tokens from datastore.

--- project/logout.py
+++ project/logout.py
@@ -13,8 +13,6 @@ template_env = jinja2.Environment(
 class LogoutHandler(BaseHandler):
 	@login_required
 	def get(self):
-		self.session['term'] = None
-		self.session['syllabus'] = None
-		self.session['user'] = None
-		self.auth.unset_session
+		self.auth.unset_session()
+		self.session.clear()
 		self.redirect('/login')
