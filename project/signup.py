c4ca3231c4b53f5e5304392a01a48c971a940768
Scott Ehlert <ds@alliedmods.net>
2015-12-05 20:21:41 -0600

    Add error messages to signup page.

--- project/signup.py
+++ project/signup.py
@@ -1,5 +1,6 @@
 import webapp2
 import jinja2
+import re
 import os
 from webapp2_extras import auth
 
@@ -26,31 +27,45 @@ class SignupForm(Form):
 '''
 
 class SignupHandler(BaseHandler):
-    def get(self):
+    def render(self, errors = None, username = ''):
         template = template_env.get_template('signup.html')
         context = {
-            
+            'errors': errors,
+            'username': username
         }
         self.response.write(template.render(context))
-        
+
+    def get(self):
+        self.render()
+
     def post(self):
         username = self.request.get('usernameSignup')
         email = self.request.get('emailSignup')
         password = self.request.get('passwordSignup')
         confirm = self.request.get('confirmSignup')
-        
+        errors = []
+
+        if not username:
+            errors.append('Username field must not be empty')
+        elif len(username) < 3:
+            errors.append('Username must be at least 3 characters long')
+        elif self.auth.store.user_model.get_by_auth_id(username):
+            errors.append('Username "' + username + '" already exists')
+        elif not re.match('^[\w_]+$', username):
+            errors.append('Username must only contain alphanumeric characters (letters A-Z or numbers 0-9) or underscores (_)')
+        if not password:
+            errors.append('Password field must not be empty')
+        elif len(password) < 8:
+            errors.append('Password must be at least 8 characters long')
         if password != confirm:
-            self.redirect('/signup')
-            return
-        
+            errors.append('Password and Confirm Password fields must match')
+
+        if errors:
+            return self.render(errors, username)
+
         success, info = self.auth.store.user_model.create_user(username, password_raw = password)
-        
+
         if success:
             return self.redirect('/login')
-        else:
-            '''
-            Errors errors errors
-            '''
-            print "error"
-        
-        self.redirect('/signup')
+
+        return self.render(['Unexpected error creating user'])
