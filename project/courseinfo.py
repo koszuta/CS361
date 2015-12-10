395b10533f50be0f348c5cc7343aac049ef2921c
koszuta <koszutan94@gmail.com>
2015-12-09 22:33:29 -0600

    Added a "Rescrape" button to Course Info Edit

--- project/courseinfo.py
+++ project/courseinfo.py
@@ -3,7 +3,7 @@ import jinja2
 import os
 from google.appengine.ext import ndb
 
-import webscrape
+from webscrape import WebScraper
 
 class Info(ndb.Model):
     title = ndb.StringProperty()
@@ -29,6 +29,23 @@ class Info(ndb.Model):
         
         return abbr + str(self.number) + "-" + str(self.section)
     
+    @webapp2.cached_property
+    def number_string(self):
+        if self.number < 10:
+            return '00' + str(self.number)
+        elif self.number < 100:
+            return '0' + str(self.number)
+        else:
+            return str(self.number)
+        
+    @webapp2.cached_property
+    def section_string(self):
+        if self.section < 10:
+            return '00' + str(self.section)
+        elif self.section < 100:
+            return '0' + str(self.section)
+        else:
+            return str(self.section)
     
 from basehandler import BaseHandler, login_required
 
@@ -51,45 +68,79 @@ class InfoEditHandler(BaseHandler):
         
     @login_required
     def post(self):
+        termKey = self.session.get('term')
+        term = ndb.Key(urlsafe = termKey).get()
         syllabusKey = self.session.get('syllabus')
         syllabus = ndb.Key(urlsafe = syllabusKey).get()
         
+        select = self.request.get('button')
         subject = str(self.request.get('subjectSelect'))
-        syllabus.info.subject = subject
-        
         number = int(self.request.get('courseNumber'))
-        syllabus.info.number = number
-        
         section = int(self.request.get('sectionNumber'))
+        
+        syllabus.info.subject = subject
+        syllabus.info.number = number            
         syllabus.info.section = section
         
-        title = str(self.request.get('courseTitle'))
-        syllabus.info.title = title
-        
-        days = ""
-        monday = str(self.request.get('mondayCheck'))
-        days = days + monday
-        tuesday = str(self.request.get('tuesdayCheck'))
-        days = days + tuesday
-        wednesday = str(self.request.get('wednesdayCheck'))
-        days = days + wednesday
-        thursday = str(self.request.get('thursdayCheck'))
-        days = days + thursday
-        friday = str(self.request.get('fridayCheck'))
-        days = days + friday
-        syllabus.info.days = days
-        
-        start = str(self.request.get('startTime'))
-        syllabus.info.start = start
-        
-        end = str(self.request.get('endTime'))
-        syllabus.info.end = end
-        
-        building = str(self.request.get('meetingBuilding'))
-        syllabus.info.building = building
-        
-        room = str(self.request.get('meetingRoom'))
-        syllabus.info.room = room
+        if select == 'Rescrape':
+            course_list = WebScraper.scrapeCourseNames(str(term.semester), int(term.year), syllabus.info.subject)
+            course_name = None
+            for c in course_list:
+                if WebScraper.getCourseSubjectFromCourseName(c) == str(syllabus.info.subject) and WebScraper.getCourseNumberFromCourseName(c) == syllabus.info.number_string:
+                    course_name = c
+                    break
+                    
+            if course_name:
+                syllabus.info.title = WebScraper.getCourseTitleFromCourseName(course_name)
+            
+            sections_list = WebScraper.scrapeCourseSections(str(term.semester), int(term.year), course_name)
+            course_section = None
+            for s in sections_list:
+                if WebScraper.getSectionFromCourseSection(s) == 'LEC ' + syllabus.info.section_string:
+                    course_section = s
+                    break
+                    
+            if course_section:
+                room = WebScraper.getRoomFromCourseSection(course_section)
+                if room:
+                    syllabus.info.building = room.split()[0]
+                    syllabus.info.room = room.split()[1]
+                    
+                syllabus.info.days = WebScraper.getMeetDaysFromCourseSection(course_section)
+                
+                time = WebScraper.getMeetTimeFromCourseSection(course_section)
+                if time:
+                    syllabus.info.start = time.split()[0]
+                    syllabus.info.end = time.split('-')[1].split()[0]
+            
+        else:            
+            title = str(self.request.get('courseTitle'))
+            syllabus.info.title = title
+            
+            days = ""
+            monday = str(self.request.get('mondayCheck'))
+            days = days + monday
+            tuesday = str(self.request.get('tuesdayCheck'))
+            days = days + tuesday
+            wednesday = str(self.request.get('wednesdayCheck'))
+            days = days + wednesday
+            thursday = str(self.request.get('thursdayCheck'))
+            days = days + thursday
+            friday = str(self.request.get('fridayCheck'))
+            days = days + friday
+            syllabus.info.days = days
+            
+            start = str(self.request.get('startTime'))
+            syllabus.info.start = start
+            
+            end = str(self.request.get('endTime'))
+            syllabus.info.end = end
+            
+            building = str(self.request.get('meetingBuilding'))
+            syllabus.info.building = building
+            
+            room = str(self.request.get('meetingRoom'))
+            syllabus.info.room = room
         
         syllabus.put()
         
